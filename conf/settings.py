961aa325cc12bfe9f5c41ca97eb41d6e3366e21d
Doston K <abdurahmonovjamshid060@gmail.com>
2025-09-05 08:45:25 +0500

    added req

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2ix^y=m$z+^mv#*))ao$m&%ppnb^)(#0v*kt5@qh5-otc7m8h2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -112,10 +112,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+MEDIA_URLS = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
